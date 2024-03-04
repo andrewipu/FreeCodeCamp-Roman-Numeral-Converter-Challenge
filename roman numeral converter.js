20667ea7eb56b785217e2a03cc85848e7ffda947
Andrew Ipu <ipuandrew@gmail.com>
2024-03-04 16:13:42 +0300

    At this point - I'm able to infer the object and grab corresponding value

--- roman numeral converter.js
+++ roman numeral converter.js
@@ -1,5 +1,33 @@
 function convertToRoman(num) {
-    return num;
+    //dictionary to held key/val pairs
+    const numerals = {
+        1 : "I",
+        4 : "IV",
+        5 : "V",
+        9 : "IX",
+        10 : "X",
+        40 : "XL",
+        50 : "L",
+        90 : "XC",
+        100 : "C",
+        400 : "CD",
+        500 : "D",
+        900 : "CM",
+        1000 : "M"
+    };
+    let infNumerals = [];
+    let result;
+
+    //infer dictionary and grab corresponding
+    for ([key, val] of Object.entries(numerals)) {
+        console.log(val);
+        console.log(key);
+        if (key == num) {
+            result = val;
+            console.log(val);
+        }
+    }
+
    }
    
-   convertToRoman(36);
+   convertToRoman(1);
