3b47bf05e14edb96e339831c0afaac76fa26ffa6
Andrew Ipu <ipuandrew@gmail.com>
2024-03-05 12:56:28 +0300

    Done

--- roman numeral converter.js
+++ roman numeral converter.js
@@ -1,33 +1,38 @@
 function convertToRoman(num) {
-    //dictionary to held key/val pairs
     const numerals = {
-        1 : "I",
-        4 : "IV",
-        5 : "V",
-        9 : "IX",
-        10 : "X",
-        40 : "XL",
-        50 : "L",
-        90 : "XC",
-        100 : "C",
-        400 : "CD",
-        500 : "D",
-        900 : "CM",
-        1000 : "M"
+        1: "I",
+        4: "IV",
+        5: "V",
+        9: "IX",
+        10: "X",
+        40: "XL",
+        50: "L",
+        90: "XC",
+        100: "C",
+        400: "CD",
+        500: "D",
+        900: "CM",
+        1000: "M"
     };
+    //Save inferred numerals
     let infNumerals = [];
-    let result;
+    
+    //Save Roman Numerals 
+    let romanNumerals = [];
 
-    //infer dictionary and grab corresponding
-    for ([key, val] of Object.entries(numerals)) {
-        console.log(val);
-        console.log(key);
-        if (key == num) {
-            result = val;
-            console.log(val);
+    for (let key of Object.keys(numerals).sort((a, b) => b - a)) {
+        //convert key to integer
+        let value = parseInt(key);
+        while (num >= value) {
+            infNumerals.push(numerals[key]);
+            num -= value;
         }
     }
 
-   }
-   
-   convertToRoman(1);
+    //Concatenate and return the numerals as a string
+    romanNumerals = infNumerals.join('');
+    return romanNumerals;
+
+}
+
+convertToRoman(4);
